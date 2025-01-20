ef956adcaee4295f59566e3539533011c666d1f7
sontungnp <75358472+sontungnp@users.noreply.github.com>
2025-01-21 00:15:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -24,32 +24,26 @@ document.addEventListener("DOMContentLoaded", () => {
         
 
         function updateAndRefreshData() {
-            //const dashboard = tableau.extensions.dashboardContent.dashboard;
+            const startDate = document.getElementById('startDate').value;
+            const endDate = document.getElementById('endDate').value;
+	    // Chuyển giá trị sang định dạng yyyy-mm-dd
+            const formattedStartDate = new Date(startDate).toISOString().split('T')[0];
+            const formattedEndDate = new Date(endDate).toISOString().split('T')[0];
+	    // Nối hai giá trị với dấu phẩy
+            const fromDateToDate = `${formattedStartDate},${formattedEndDate}`;
 
-            // Get the parameters P_FROM_DATE and P_TO_DATE
+            // Get the parameters P_Fd_Td
             dashboard.getParametersAsync().then(parameters => {
-                const fromDateParam = parameters.find(param => param.name === 'P_From_Date');
-                const toDateParam = parameters.find(param => param.name === 'P_To_Date');
-		        const fdtdParam = parameters.find(param => param.name === 'P_Fd_Td');
-				
-                if (!fromDateParam || !toDateParam) {
-                    document.getElementById('status').innerText = "Missing parameters: P_From_Date or P_To_Date.";
-                    return;
-                }
-
-                const fromDate = fromDateParam.currentValue.value;
-                const toDate = toDateParam.currentValue.value;
-				const fromDateToDate = fromDate + ',' + toDate
-				
-				// Cập nhật P_FD bằng giá trị của P_From_Date + ',' + P_To_Date
-				fdtdParam.changeValueAsync(fromDateToDate).then(() => {
+                const fdtdParam = parameters.find(param => param.name === 'P_Fd_Td');
+		// Cập nhật P_FD bằng giá trị của fromDateToDate
+		fdtdParam.changeValueAsync(fromDateToDate).then(() => {
                                 console.log('thay doi tham so P_Fd_Td');
-                            });
+                });
             }).catch(err => {
                 console.error("Đã có lỗi xảy ra. Đảm bảo có đủ tham số: P_From_Date (date), P_To_Date (Date), P_Fd_Td (String).");
                 alert("Đã có lỗi xảy ra. Đảm bảo có đủ tham số: P_From_Date (date), P_To_Date (Date), P_Fd_Td (String)");
-                console.error("P_Fd_Td = " + fromDateToDate);
-                alert("P_Fd_Td = " + fromDateToDate);
+                // console.error("P_Fd_Td = " + fromDateToDate);
+                // alert("P_Fd_Td = " + fromDateToDate);
                 console.error(err);
             });
         }
