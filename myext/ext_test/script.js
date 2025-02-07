461aafbd1a9a209b7719e02e17b4b4f7e23eab65
sontungnp <75358472+sontungnp@users.noreply.github.com>
2025-02-07 21:43:49 +0700

    Update script.js

--- myext/ext_test/script.js
+++ myext/ext_test/script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log("Extension initialized");
 
         document.getElementById("openPopup").addEventListener("click", () => {
-            let popupUrl = window.location.origin + "/popup.html"; // URL của file popup
+            let popupUrl = window.location.origin + "/tableau/myext/ext_config/popup.html"; // URL của file popup
             tableau.extensions.ui.displayDialogAsync(popupUrl, "", { width: 400, height: 300 })
                 .then((payload) => {
                     console.log("Popup đóng với dữ liệu: " + payload);
